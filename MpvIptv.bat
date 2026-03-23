9b4a3caaf74863874cf3de707a6a5dfac30e7c9b
check4game <maxim.amsd@gmail.com>
2026-03-23 19:29:54 +0300

    v1.4.6

--- MpvIptv.bat
+++ MpvIptv.bat
@@ -11,7 +11,7 @@ if %errorlevel% equ 0 (
 	set exec=powershell
 )
 
-%exec% -Command "& { Write-Host "!useragent! script v1.4.5" -ForegroundColor Green; }"
+%exec% -Command "& { Write-Host "!useragent! script v1.4.6" -ForegroundColor Green; }"
 
 set 7zrUrl=https://www.7-zip.org/a/7zr.exe
 set 7zaUrl=https://www.7-zip.org/a/7z2600-extra.7z
@@ -227,7 +227,7 @@ set "etag=!etag:/=.!.etag"
 
 if exist "!tempPath!\!etag!" (
 	%exec% -Command "& { Write-Host "Updating %~1" -ForegroundColor Green; }"
-	"!EXECURL!" --compressed --no-progress-meter --user-agent "!useragent!" --etag-save "!tempPath!\!etag!" --etag-compare "!tempPath!\!etag!" -RLo "!configPath!\%~1" --fail -z "!configPath!\%~1" "!configUrl!/%~1"
+	"!EXECURL!" --compressed --no-progress-meter --user-agent "!useragent!" --etag-save "!tempPath!\!etag!" --etag-compare "!tempPath!\!etag!" -RLo "!configPath!\%~1" --fail "!configUrl!/%~1"
 ) else (
 	%exec% -Command "& { Write-Host "Downloading %~1" -ForegroundColor Green; }"
 	"!EXECURL!" --compressed --no-progress-meter --user-agent "!useragent!" --etag-save "!tempPath!\!etag!" -RLo "!configPath!\%~1" --fail "!configUrl!/%~1"
